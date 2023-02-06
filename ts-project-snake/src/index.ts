3900a20f17744c40386224901c58a2bab28671f1
v-liujf25 <17858343@qq.com>
2023-02-06 23:18:35 +0800

    feat 计分类

--- ts-project-snake/src/index.ts
+++ ts-project-snake/src/index.ts
@@ -1,9 +1,7 @@
 import './style/index.less'
-console.log(123)
 
-// 食物类
 /**
- * 
+ * 食物类
  */
 class Food {
     // 定义一个属性表示食物所应对的元素
@@ -34,3 +32,43 @@ class Food {
 const food = new Food()
 console.log(food.X, food.Y, 'food的x y坐标')
 food.change()
+
+/**
+ * 计分牌类
+ */
+class ScorePanel {
+    // 用来记录分数
+    score: number = 0
+    // 用来记录等级
+    level: number = 1
+    scoreEle: HTMLElement
+    levelEle: HTMLElement
+
+    // 控制游戏变量
+    maxLevel: number
+    upScore: number
+
+    constructor (maxLevel: number = 10, upScore: number = 10) {
+        this.scoreEle = document.getElementById('score')!
+        this.levelEle = document.getElementById('level')!
+        this.maxLevel = maxLevel
+        this.upScore = upScore
+    }
+
+    // 加分
+    addScore () {
+        this.score++
+        this.scoreEle.innerHTML = 'SCORE:' + this.score
+        // 分数够了就能升一级
+        if (this.score % this.upScore === 0) this.levelUp()
+    }
+
+    // 等级提升
+    levelUp () {
+        if (this.level < this.maxLevel) this.levelEle.innerHTML = 'LEVEL:' + ++this.level
+    }
+}
+
+const scorePanel = new ScorePanel()
+scorePanel.addScore()
+scorePanel.levelUp()
