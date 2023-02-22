8f144652774259220947b29e7ea6a8c8e57d65a9
v-liujf25 <17858343@qq.com>
2023-02-22 23:40:39 +0800

    feat 蛇移动身体

--- ts-project-snake/src/modules/Snake.ts
+++ ts-project-snake/src/modules/Snake.ts
@@ -1,3 +1,4 @@
+// 设计类尽量遵循单一职责原则
 class Snake {
     head: HTMLElement // 蛇头
     bodies: HTMLCollection // 蛇身 包含蛇头 HTMLCollection会自动补充新元素
@@ -23,18 +24,38 @@ class Snake {
         if (this.X === value) return
         // 也可以在此处做X/Y值判断 超出边界则
         // if (value < 0 || value > 300) throw new Error('蛇撞墙了!')
+        // 移动身体
+        this.moveBody()
+
         this.head.style.left = value + 'px'
     }
     set Y (value: number) {
         if (this.Y === value) return
         // 也可以在此处做X/Y值判断 超出边界则
         // if (value < 0 || value > 310) throw new Error('蛇撞墙了!')
+        // 移动身体
+        this.moveBody()
+
         this.head.style.top = value + 'px'
     }
 
     // 增加身体长度(吃食后) 往#snake添加元素
     addBody () {
         this.element.insertAdjacentHTML('beforeend', '<div></div>')
+    }
+    // 添加身体长度后还需要身体移动
+    // 蛇头去新位置 后一节去上一节位置
+    moveBody () {
+        // i = 0 就改到头了
+        for(let i = this.bodies.length - 1; i > 0; i--) {
+            // 获取前边身体的位置
+            let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+            let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+
+            // 将值设置到当前身体上
+            (this.bodies[i] as HTMLElement).style.left = X + 'px';
+            (this.bodies[i] as HTMLElement).style.top = Y + 'px';
+        }
     }
 }
 export default Snake
